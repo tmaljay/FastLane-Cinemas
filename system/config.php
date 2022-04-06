5b262cb81da9f182dd5a574cab88aafd4cb78ac1
tmaljay <44429138+tmaljay@users.noreply.github.com>
2022-04-06 16:00:05 +0100

    Create config.php

---
+++ system/config.php
@@ -0,0 +1,19 @@
+<?php
+	define("URL", "http://localhost:8888/scripts/Moviepoint/movie-ticket-booking-website-php-and-mysql-v3/");
+	define("VIEW", "views/");
+    define("PUBLIC_PATH", URL . "public/");
+	define("CSS", URL . "public/css/");
+	define("JS", URL . "public/js/");
+	define("IMG", URL . "public/img/");
+
+    define("ADMIN_VIEW", VIEW . "admin/");
+    define("ADMIN_CSS", URL . "public/admin/css/");
+    define("ADMIN_JS", URL . "public/admin/js/");
+    define("ADMIN_IMG", URL . "public/admin/img/");
+    define("ADMIN_VENDOR", URL . "public/admin/vendors/");
+	
+	define("DB_HOST", "localhost:8889");
+	define("DB_USER", "root");
+	define("DB_PASS", "root");
+	define("DB_NAME", "moviepoint_v3");
+?>
